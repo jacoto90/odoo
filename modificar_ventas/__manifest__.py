49f5aea58dd83a8d7062ffbd6831326c1b6f3d65
jacoto90 <115584355+jacoto90@users.noreply.github.com>
2023-05-19 18:29:39 +0200

    modulo para modificar una vista de ventas
    modifica el visual de la vista de ventas con una herencia

---
+++ modificar_ventas/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Modificar Ventas',
+    'summary': 'Modificaciones a la vista de órdenes de venta',
+    'description': 'Añade campos y modificaciones a la vista de órdenes de venta',
+    'author': 'Tu nombre',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
